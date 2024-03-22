af43b82af018e80faa90f0b8234eb61e3fe5d9b0
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-03-22 11:32:27 +0500

    Shahab Munir Portfolio

---
+++ src/components/layout/header/Offcanvas.tsx
@@ -0,0 +1,150 @@
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "public/images/logo.png";
+
+interface HeaderProps {
+  openNav: boolean;
+  setOpenNav: (value: boolean) => void;
+}
+
+const Offcanvas = ({ openNav, setOpenNav }: HeaderProps) => {
+  const [openSubMenu, setOpenSubMenu] = useState<string | null>(null);
+  const [openNestedMenu, setOpenNestedMenu] = useState<string | null>(null);
+
+  const handleSubmenu = (submenu: string) => {
+    if (submenu === openSubMenu) {
+      setOpenSubMenu(null);
+    } else {
+      setOpenSubMenu(submenu);
+    }
+  };
+
+  const handleNestedmenu = (nestmenu: string) => {
+    if (nestmenu === openNestedMenu) {
+      setOpenNestedMenu(null);
+    } else {
+      setOpenNestedMenu(nestmenu);
+    }
+  };
+
+  const isNestedMenuOpen = (nestmenu: string) => {
+    return nestmenu === openNestedMenu ? " nav__dropdown-active" : " ";
+  };
+
+  const isNestedMenuButton = (nestmenu: string) => {
+    return nestmenu === openNestedMenu ? " navbar__item-active" : " ";
+  };
+
+  const isSubMenuOpen = (submenu: string) => {
+    return submenu === openSubMenu ? " nav__dropdown-active" : " ";
+  };
+
+  const isSubMenuButton = (submenu: string) => {
+    return submenu === openSubMenu ? " navbar__item-active" : " ";
+  };
+
+  // window resize
+  useEffect(() => {
+    const handleResizeHeader = (): void => {
+      setOpenNav(false);
+      setOpenSubMenu(null);
+    };
+
+    window.addEventListener("resize", handleResizeHeader);
+
+    return () => {
+      window.removeEventListener("resize", handleResizeHeader);
+    };
+  }, []);
+
+  const closeNav = () => {
+    setOpenNav(false);
+    setOpenSubMenu(null);
+  };
+
+  return (
+    <div className="offcanvas-nav">
+      <div
+        className={"offcanvas-menu" + (openNav ? " show-offcanvas-menu" : " ")}
+      >
+        <nav className="offcanvas-menu__wrapper" data-lenis-prevent>
+          <div className="offcanvas-menu__header nav-fade">
+            <div className="logo">
+              <Link href="/">
+                <Image src={logo} alt="Image" title="Image" priority />
+              </Link>
+            </div>
+            <button
+              aria-label="close offcanvas menu"
+              className="close-offcanvas-menu"
+              onClick={closeNav}
+            >
+              <i className="fa-light fa-xmark-large"></i>
+            </button>
+          </div>
+          <div className="offcanvas-menu__list">
+            <div className="navbar__menu">
+              <ul>
+                <li className="navbar__item nav-fade">
+                  <Link href="/">Home</Link>
+                </li>
+                <li className="navbar__item nav-fade">
+                  <Link href="about-us">About</Link>
+                </li>
+                <li className="navbar__item nav-fade">
+                  <Link href="about-us">Services</Link>
+                </li>
+                <li className="navbar__item nav-fade">
+                  <Link href="about-us">Portfolio</Link>
+                </li>
+                <li className="navbar__item nav-fade">
+                  <Link href="about-us">Contact</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="offcanvas-menu__options nav-fade">
+            <div className="offcanvas__mobile-options d-flex">
+              <Link href="contact-us" className="btn btn--secondary">
+                Let&apos;s Talk
+              </Link>
+            </div>
+          </div>
+          <div className="offcanvas-menu__social social nav-fade">
+            <Link
+              href="https://www.facebook.com/"
+              target="_blank"
+              aria-label="share us on facebook"
+            >
+              <i className="fa-brands fa-facebook-f"></i>
+            </Link>
+            <Link
+              href="https://www.twitter.com/"
+              target="_blank"
+              aria-label="share us on twitter"
+            >
+              <i className="fa-brands fa-twitter"></i>
+            </Link>
+            <Link
+              href="https://www.pinterest.com/"
+              target="_blank"
+              aria-label="share us on pinterest"
+            >
+              <i className="fa-brands fa-linkedin-in"></i>
+            </Link>
+            <Link
+              href="https://www.instagram.com/"
+              target="_blank"
+              aria-label="share us on instagram"
+            >
+              <i className="fa-brands fa-instagram"></i>
+            </Link>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Offcanvas;
