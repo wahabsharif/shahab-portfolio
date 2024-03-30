73e6977641581df7a4b8a199e7852f82320fab5f
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-03-30 12:02:21 +0500

    Contact Page

--- src/components/containers/ContactMain.tsx
+++ src/components/containers/ContactMain.tsx
@@ -10,19 +10,18 @@ const ContactMain = () => {
   return (
     <section className="section contact-m fade-wrapper">
       <div className="container">
-        <div className="row gaper">
+        {/* <div className="row gaper">
           <div className="col-12 col-sm-6 col-xl-3">
             <div className="contact-m__single topy-tilt fade-top">
               <div className="thumb">
                 <Image src={phone} alt="Image" />
               </div>
               <div className="content">
-                <h4>Phone & Fax</h4>
+                <h4>Phone</h4>
                 <p>
-                  <Link href="tel:197-90-56-780">Mobile : +197-90-56-780</Link>
-                </p>
-                <p>
-                  <Link href="tel:197-90-56-780">Fax : +44-208-1234567</Link>
+                  <Link href="tel:+923177906015">
+                    Mobile : +92-317-790-6015
+                  </Link>
                 </p>
               </div>
             </div>
@@ -36,12 +35,7 @@ const ContactMain = () => {
                 <h4>Mail Address</h4>
                 <p>
                   <Link href="mailto:info.company@gmail.com">
-                    Info.company@gmail.com
-                  </Link>
-                </p>
-                <p>
-                  <Link href="mailto:info.company@gmail.com">
-                    Info.company@yahoo.com
+                    shahabmunir98@gmail.com
                   </Link>
                 </p>
               </div>
@@ -77,12 +71,12 @@ const ContactMain = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
         <div className="row">
           <div className="col-12">
             <div className="map-wrapper">
-              <div className="row gaper">
-                <div className="col-12 col-lg-6">
+              <div className="row gaper justify-content-center">
+                {/* <div className="col-12 col-lg-6">
                   <div className="contact__map fade-top">
                     <iframe
                       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20342.411046372905!2d-74.16638039276373!3d40.719832743885284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1649562691355!5m2!1sen!2sbd"
@@ -94,7 +88,7 @@ const ContactMain = () => {
                       referrerPolicy="no-referrer-when-downgrade"
                     ></iframe>
                   </div>
-                </div>
+                </div> */}
                 <div className="col-12 col-lg-6">
                   <div className="contact-main__form  fade-top">
                     <h3>Leave A Message</h3>
